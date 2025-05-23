29e73088c8b4ae5590d52f53de25b424c7c15ad8
notZairus <notzairus@gmail.com>
2025-05-23 17:24:44 +0800

    responsiveness

--- src/components/MainInterface.tsx
+++ src/components/MainInterface.tsx
@@ -3,7 +3,7 @@ import Webcam from 'react-webcam';
 
 function MainInterface() {
   return (
-    <div className="w-screen h-screen min-h-screen bg-white box-border p-8 flex gap-4">
+    <div className="w-screen h-screen min-h-screen bg-white box-border p-8 flex gap-4 flex-col lg:flex-row">
       <div className='flex-1 flex flex-col'>
         <div className='w-full aspect-video flex items-center justify-center'>
           <Webcam 
